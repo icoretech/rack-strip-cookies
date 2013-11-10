a3046277c952ac15095ad5fc0592cd3de334a7d0
Claudio Poli <masterkain@gmail.com>
2013-11-10 18:20:52 +0100

    erase request cookie as well

--- lib/rack/strip-cookies.rb
+++ lib/rack/strip-cookies.rb
@@ -8,15 +8,13 @@ module Rack
 
     def call(env)
       path = Rack::Request.new(env).path
+      env.delete('HTTP_COOKIE') if paths.include?(path)
       status, headers, body = @app.call(env)
-
-      if paths.include?(path)
-        headers.delete('Set-Cookie')
-        # request.session_options[:skip] = true
-        # env.delete('HTTP_COOKIE')
-      end
-
-      [status, headers, body]
+      headers.delete('Set-Cookie') if paths.include?(path)
+      # request.session_options[:skip] = true
+      # env.delete('HTTP_COOKIE')
     end
+
+    [status, headers, body]
   end
 end

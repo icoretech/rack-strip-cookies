ccd161fa2c39da1da3a2a8fb591952d35d60574c
Siddharth Patel <sidcpatel@users.noreply.github.com>
2018-04-11 12:55:30 -0700

    add an option to inverse the behavior
    i.e. allow user to strip out cookies from all requests except those
    mentioned in paths

--- spec/rack-strip-cookies_spec.rb
+++ spec/rack-strip-cookies_spec.rb
@@ -30,17 +30,31 @@ describe Rack::StripCookies do
     @app = builder.to_app
   end
 
-  before do
-    mock_app(paths: ["/oauth/token"])
-  end
-
   it 'does not clean the cookie on another path' do
+    mock_app(paths: ["/oauth/token"])
+
     get 'http://www.example.org/oauth'
     assert_equal last_response.headers['Set-Cookie'].split("\n"), ["id=1; path=/oauth/token; secure; HttpOnly"], "cookie is present"
   end
 
   it 'clean the cookie' do
+    mock_app(paths: ["/oauth/token"])
+
     get 'http://www.example.org/oauth/token'
     assert_nil last_response.headers['Set-Cookie'], "cookie is missing"
+  end
+
+  it 'clean the cookie on all other paths' do
+    mock_app(paths: ["/oauth/token"], invert: true)
+
+    get "http://www.example.org/outh#{rand(10)}"
+    assert_nil last_response.headers['Set-Cookie'], "cookie is missing"
+  end
+
+  it 'dont clean the cookie on given path' do
+    mock_app(paths: ["/oauth/token"], invert: true)
+
+    get 'http://www.example.org/oauth/token'
+    assert_equal last_response.headers['Set-Cookie'].split("\n"), ["id=1; path=/oauth/token; secure; HttpOnly"], "cookie is present"
   end
 end
